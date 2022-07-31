580c3a37815e36e33f14d3539af2cafd8909acb0
tokaiknight <tokaiknight@yahoo.co.jp>
2022-08-01 03:14:00 +0900

    no message

--- kit.src.js
+++ kit.src.js
@@ -5,17 +5,34 @@ import {html, render} from 'https://unpkg.com/htm/preact/standalone.module.js'
 function kit(self){
     if(!self.shadowRoot){
         self.attachShadow({mode:'open'})
-
         self.$ = new Proxy({}, {get: (_,name) => self.shadowRoot.querySelector(`#${name}`)})
 
-        for(const v of Object.getOwnPropertyNames(self.constructor.prototype)){
-            if(typeof self[v] === 'function'){
-                self[v] = self[v].bind(self)
+        const method = Object.getOwnPropertyNames(self.constructor.prototype).filter(v => typeof self[v] === 'function')
+        method.forEach(v => self[v] = self[v].bind(self))
+
+        const dom = self.html()
+        self.vdom = dom.constructor === undefined || Array.isArray(dom)
+
+        if(self.vdom){
+            render(dom, self.shadowRoot)
+        }
+        else if(typeof dom === 'string'){
+            self.shadowRoot.innerHTML = dom
+        }
+        else{
+            self.shadowRoot.append(dom)
+        }
+
+        for(const v of method){
+            const match = v.match(/^\$(.*?)_([^_]+)$/)
+            if(match){
+                self.$[match[1]]?.addEventListener(match[2], self[v])
             }
         }
     }
-
-    render(self.html(), self.shadowRoot)
+    else if(self.vdom){
+        render(self.html(), self.shadowRoot)
+    }
 }
 
 kit.h = html
