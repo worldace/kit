7158a0c3d629bb724e1472c169043c1c6fff776e
tokaiknight <tokaiknight@yahoo.co.jp>
2022-08-01 04:40:26 +0900

    no message

--- kit.src.js
+++ kit.src.js
@@ -5,7 +5,10 @@ import {html, render} from 'https://unpkg.com/htm/preact/standalone.module.js'
 function kit(self){
     if(!self.shadowRoot){
         self.attachShadow({mode:'open'})
-        self.$ = new Proxy({}, {get: (_,name) => self.shadowRoot.querySelector(`#${name}`)})
+        self.$ = new Proxy(function(){}, {
+            get: (_,name) => self.shadowRoot.querySelector(`#${name}`),
+            apply: (_,__,args) => querySelector(self.shadowRoot, args[0]),
+        })
 
         const method = Object.getOwnPropertyNames(self.constructor.prototype).filter(v => typeof self[v] === 'function')
         method.forEach(v => self[v] = self[v].bind(self))
@@ -34,6 +37,20 @@ function kit(self){
         render(self.html(), self.shadowRoot)
     }
 }
+
+
+function querySelector(root, selector){
+    if(selector.startsWith('*')){
+        if(selector.length > 1){
+            selector = selector.substring(1)
+        }
+        return root.querySelectorAll(selector)
+    }
+    else{
+        return root.querySelector(selector)
+    }
+}
+
 
 kit.h = html
 
