648522fa5c3b565ba657a6833e666c39a5375b81
tokaiknight <tokaiknight@yahoo.co.jp>
2022-07-31 12:01:23 +0900

    no message

--- kit.src.js
+++ kit.src.js
@@ -1,14 +1,12 @@
 // https://cdn.jsdelivr.net/gh/worldace/kit/kit.js
 
-import {html, render} from "https://unpkg.com/htm/preact/standalone.module.js"
+import {html, render} from 'https://unpkg.com/htm/preact/standalone.module.js'
 
 function kit(self){
     if(!self.shadowRoot){
         self.attachShadow({mode:'open'})
 
-        self.$ = new Proxy({}, {'get':function(target,name){
-            return self.shadowRoot.querySelector(`#${name}`)
-        }})
+        self.$ = new Proxy({}, {get: (_,name) => self.shadowRoot.querySelector(`#${name}`)})
 
         for(const v of Object.getOwnPropertyNames(self.constructor.prototype)){
             if(typeof self[v] === 'function'){
@@ -16,6 +14,7 @@ function kit(self){
             }
         }
     }
+
     render(self.html(), self.shadowRoot)
 }
 
