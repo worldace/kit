4bdf675a598e47682ca40c6538c606e9d7403147
tokaiknight <tokaiknight@yahoo.co.jp>
2023-11-27 19:03:23 +0900

    no message

--- kit.src.js
+++ kit.src.js
@@ -1,112 +1,72 @@
-// https://cdn.jsdelivr.net/gh/worldace/kit/kit.js
-
 import {html, render} from 'https://unpkg.com/htm@3.1.1/preact/standalone.module.js'
 
-
+// https://qiita.com/economist/items/2c45b992f78481ef0a08
 
 function kit(self, ...vars){
     if(Array.isArray(self)){
         return html(self, ...vars)
     }
+    else if(self.vDOM){
+        render(self.html(), self.shadowRoot)
+    }
     else if(!self.shadowRoot){
         self.attachShadow({mode:'open'})
-        self.$ = new Proxy(function(){}, {get:get.bind(self), apply:apply.bind(self)})
+        self.$ = new Proxy(function(){}, {
+            get  : (_, name) => self.shadowRoot.querySelector('#'+name),
+            apply: apply.bind(self),
+        })
 
-        const method = Object.getOwnPropertyNames(self.constructor.prototype).filter(v => typeof self[v] === 'function' && v !== 'constructor')
-        method.forEach(v => self[v] = self[v].bind(self))
+        const methods = Object.getOwnPropertyNames(Object.getPrototypeOf(self))
+        methods.forEach(method => self[method] = self[method].bind(self))
 
-        if(self.css && document.adoptedStyleSheets){
-            let css = kit.StyleSheets.get(self.constructor)
-            if(!css){
-                css = new CSSStyleSheet()
-                css.replaceSync(self.css())
-                kit.StyleSheets.set(self.constructor, css)
+        if(self.css){
+            if(!self.constructor.css){
+                self.constructor.css = new CSSStyleSheet()
+                self.constructor.css.replaceSync(self.css())
             }
-            self.shadowRoot.adoptedStyleSheets = [css]
+            self.shadowRoot.adoptedStyleSheets = [self.constructor.css]
         }
 
-        let dom = self.html()
-        self.isVDOM = dom.constructor === undefined || Array.isArray(dom)
-
-        if(self.isVDOM){
-            if(self.css && !document.adoptedStyleSheets){
-                dom = [dom, html`<style>${self.css()}</style>`]
-            }
-            render(dom, self.shadowRoot)
-        }
-        else if(typeof dom === 'string'){
-            if(self.css && !document.adoptedStyleSheets){
-                dom += `<style>${self.css()}</style>`
-            }
+        const dom = self.html()
+        if(typeof dom === 'string'){
             self.shadowRoot.innerHTML = dom
         }
         else if(dom instanceof Node){
-            if(self.css && !document.adoptedStyleSheets){
-                self.shadowRoot.innerHTML = `<style>${self.css()}</style>`
-            }
-            if(dom.tagName === 'TEMPLATE'){
-                dom = dom.content.cloneNode(true)
-            }
-            self.shadowRoot.append(dom)
+            const el = dom.tagName === 'TEMPLATE' ? dom.content : dom
+            self.shadowRoot.append(el.cloneNode(true))
+        }
+        else if(!dom.constructor || Array.isArray(dom)){
+            self.vDOM = true
+            render(dom, self.shadowRoot)
         }
 
-        const specialID = {'':self.shadowRoot, 'Host':self, 'Window':window, 'Document':document, 'Body':document.body}
+        const specialID = {'':self.shadowRoot, 'Host':self, 'Window':window, 'Document':document}
 
-        for(const v of method){
-            const match = v.match(/^\$(.*?)_([^_]+)$/)
-            if(match){
-                const el = specialID[match[1]] ?? self.shadowRoot.querySelector(`#${match[1]}`)
-
-                if(el){
-                    el.addEventListener(match[2], self[v])
-                }
-                else{
-                    throw `kit.jsイベント登録エラー： ${self.constructor.name}.${match[0]} の登録先が存在しません`
-                }
+        for(const method of methods){
+            const m = method.match(/^\$(.*?)_([^_]+)$/)
+            if(m){
+                const el = specialID[m[1]] ?? self.$('#'+m[1])
+                el.addEventListener(m[2], self[method])
             }
         }
     }
-    else if(self.isVDOM){
-        let dom = self.html()
-        if(self.css && !document.adoptedStyleSheets){
-            dom = [dom, html`<style>${self.css()}</style>`]
-        }
-        render(dom, self.shadowRoot)
-    }
 }
-
-
-
-function get(_, name){
-    return this.shadowRoot.querySelector(`#${name}`)
-}
-
-
 
 function apply(_, __, [arg, ...values]){
     if(typeof arg === 'string'){
         if(arg.startsWith('*')){
-            return Array.from(this.shadowRoot.querySelectorAll(arg.substring(1) || '*'))
+            return Array.from(this.shadowRoot.querySelectorAll(arg.slice(1) || '*'))
         }
         else{
             return this.shadowRoot.querySelector(arg)
         }
     }
-    else if(Array.isArray(arg)){
+    else if(Array.isArray(arg)){ //タグ関数で起動
         const template = document.createElement('template')
         template.innerHTML = arg.reduce((result, v, i) => result + values[i-1] + v).trim()
 
         return template.content.childNodes.length === 1 ? template.content.firstChild : template.content
     }
-    else if(typeof arg === 'object'){
-        Object.assign(this, arg)
-        kit(this)
-    }
 }
-
-
-
-kit.StyleSheets = new Map()
-
 
 export default kit
